232d46409c90fe71b40e8bfe4306ec1ae2441331
Antonious-Stewart <antonious.stewart@gmail.com>
2019-02-09 18:29:05 -0500

    removed menu

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 document.querySelector(".container").style.display = "none";
 document.querySelector(".bg").style.display = "none";
-document.querySelector(".menu").style.display = "none";
+
 window.onload = () =>{
   setTimeout(()=>{
     document.querySelector(".load-wrapper").style.display = "none";
     document.querySelector(".container").style.display = "block";
-    document.querySelector(".menu").style.display = "block";
     document.querySelector(".bg").style.display = "block";
   },Math.floor(Math.random() * 6000) + 1000);
 }
